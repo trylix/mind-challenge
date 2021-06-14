e82fbdd1762729330f49a316d6eaf065fddbca12
Brendenson Andrade <brendensond@gmail.com>
2021-06-13 21:27:53 -0300

    backend: added get authenticated user endpoint

--- challenge-api/src/user/user.controller.ts
+++ challenge-api/src/user/user.controller.ts
@@ -1,34 +1,42 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { AuthUser } from 'src/auth/auth-user.decorator';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { LocalGuard } from 'src/auth/guards/local.guard';
 import { TransformInterceptor } from 'src/interceptors/transform.interceptor';
 import { CreateUserBodyDto } from './dto/create-user-body.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
-@Controller('users')
+@Controller()
 @UseInterceptors(new TransformInterceptor(User))
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('/users')
   async create(@Body() dto: CreateUserBodyDto) {
     const user = await this.userService.create(dto.user);
 
     return { user };
   }
 
-  @Post('login')
+  @Post('/users/login')
   @UseGuards(LocalGuard)
   login(@AuthUser() entity: User) {
     const user = this.userService.getUserWithAccessToken(entity);
 
+    return { user };
+  }
+
+  @Get('/user')
+  @UseGuards(JwtGuard)
+  currentUser(@AuthUser() user: User) {
     return { user };
   }
 }
