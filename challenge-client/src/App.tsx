a0f689e83efb2ebf6e7a059657f849a04975e5ba
Brendenson Andrade <brendensond@gmail.com>
2021-06-18 12:25:35 -0300

    frontend: update route tree

--- challenge-client/src/App.tsx
+++ challenge-client/src/App.tsx
@@ -4,8 +4,6 @@ import { Article } from './container/article';
 import { Editor } from './container/editor';
 import { Home } from './container/home';
 import { Login } from './container/login';
-import { ProfileArticles } from './container/profile/articles';
-import { ProfileFavorites } from './container/profile/favorites';
 import { Register } from './container/register';
 import { Settings } from './container/settings';
 import { Route } from './shared/components';
@@ -15,26 +13,13 @@ const App: React.FC = () => {
   return (
     <AuthProvider>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/editor" element={<Editor />} />
-            <Route path="/editor/:slug" element={<Editor />} />
-            <Route path="/article/:slug" element={<Article />} />
-            <Route path="/profile">
-              <Routes>
-                <Route path="/:username" element={<ProfileArticles />} />
-                <Route
-                  path="/:username/favorites"
-                  element={<ProfileFavorites />}
-                />
-              </Routes>
-            </Route>
-          </Routes>
-        </Route>
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/editor/:slug" element={<Editor />} />
+        <Route path="/article/:slug" element={<Article />} />
       </Routes>
     </AuthProvider>
   );
